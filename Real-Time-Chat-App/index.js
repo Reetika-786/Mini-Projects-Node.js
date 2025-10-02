b33499bf9ea9401815b631ed23f4a0161a16d28c
Reetika-786 <reetika848@gmail.com>
2025-10-02 12:40:34 +0530

    chat-app

--- Real-Time-Chat-App/index.js
+++ Real-Time-Chat-App/index.js
@@ -17,7 +17,7 @@ io.on("connection", (socket) => {
   });
 
   socket.on("disconnect", () => {
-    console.log("A user disconnected:", socket.id);
+    console.log("A user disconnected! :", socket.id);
   });
 });
 
