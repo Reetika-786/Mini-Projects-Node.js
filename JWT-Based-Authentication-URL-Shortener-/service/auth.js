81ebe6069dce7e08e593a341ef3e81e4ef1faa12
Reetika-786 <reetika848@gmail.com>
2025-10-02 12:47:06 +0530

    deleted jwt

---
+++ JWT-Based-Authentication-URL-Shortener-/service/auth.js
@@ -0,0 +1,29 @@
+//Basically, it maps session IDs to users in memory.
+// const sessionIdToUserMap = new Map();
+
+const jwt  = require('jsonwebtoken');
+const secret = 'Hey#Reetikaaaa';
+
+
+function setUser(user) {
+  // sessionIdToUserMap.set(id, user);
+
+  return jwt.sign({
+    _id : user._id,
+    email : user.email
+  }, secret);
+}
+
+function getUser(token) {
+  if(! token) return null;
+  try{
+    return jwt.verify(token, secret);
+  } catch(error) {
+    return null;
+  }
+}
+
+module.exports = {
+  setUser,
+  getUser,
+};
